106423ef04a00c6ca235b144597b46135d4d0772
18763552493@163.com <18763552493@163.com>
2022-04-26 19:45:30 +0800

    first commit

--- home/src/main/java/com/zs/home/activity/MainActivity.java
+++ home/src/main/java/com/zs/home/activity/MainActivity.java
@@ -1,18 +1,24 @@
 package com.zs.home.activity;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+
+import com.zs.home.R;
+import com.zs.home.databinding.ActivityMainBinding;
 import com.zs.home.mine.fragment.MineFragment;
 
 public class MainActivity extends AppCompatActivity {
     private Fragment mHomeFragment = new MineFragment();
     private MineFragment mAccountFragment = new MineFragment();
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        viewDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
     }
 
--- home/src/main/java/com/zs/home/mine/fragment/MineFragment.java
+++ home/src/main/java/com/zs/home/mine/fragment/MineFragment.java
@@ -4,28 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.Nullable;
-import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
-import com.zs.home.R;
-import com.zs.home.databinding.FragmentAccountBinding;
 
 public class MineFragment extends Fragment {
-    FragmentAccountBinding mBinding;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_account, container, false);
-        mBinding.mine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        return mBinding.getRoot();
+        return null;
     }
 }
